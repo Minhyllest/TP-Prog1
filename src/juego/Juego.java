75e03a5fecabf4c797efed2e4bec3ca61d993c6f
Bruno <Bruno@DESKTOP-H27IIEN>
2025-05-18 22:31:24 -0300

    Agregue los limites del fondo e hice un recuadro para el menu

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -3,29 +3,54 @@ package juego;
 import entorno.Entorno;
 import entorno.InterfaceJuego;
 import entorno.Herramientas;
+
+import java.awt.Color;
 import java.awt.Image;
+import java.util.Arrays;
 
 public class Juego extends InterfaceJuego {
 	public Image fondo; 
     public Entorno entorno;
     public Personaje personaje;
     
+    
     Juego() {
-        this.entorno = new Entorno(this, "Demo juego", 1400, 900);
-        this.fondo = Herramientas.cargarImagen("images/fondo.jpg").getScaledInstance(1400, 900, Image.SCALE_SMOOTH);
+    	int alto = 1400, ancho = 900;
+        this.entorno = new Entorno(this, "Enki's dream", alto,ancho);
+        this.fondo = Herramientas.cargarImagen("images/fondo.jpg").getScaledInstance(1400, 900, Image.SCALE_SMOOTH); // carga la imagen del fondo y lo escala a el tamaño de la pantalla.
         this.personaje = new Personaje(600, 400);
         this.entorno.iniciar(); 
+        
     }
 
     public void tick() {
-        entorno.dibujarImagen(this.fondo, 700, 450, 0); 
-        this.personaje.dibujar(entorno);
+    	
+        entorno.dibujarImagen(this.fondo, 700, 450, 0);  //background
+        
+
+        
+        this.personaje.dibujar(entorno); // dibuja al personaje.
+        
+        entorno.dibujarRectangulo(1301, 400, 200, 1600, 0, Color.black);
+        //MENU
+        entorno.dibujarRectangulo(1300, 70, 1, 150, 11, Color.WHITE);
+        entorno.dibujarRectangulo(1200, 100, 1, 1600, 0, Color.WHITE);
+        
+      
+        
+        
+        
+        entorno.cambiarFont("Chiller", 30, Color.WHITE,entorno.NEGRITA);
+        entorno.escribirTexto("Enki's Dream", 1255, 50);
+        
+        
         if (this.personaje == null) {
             return;
         }
-
+        
         boolean seMovio = false;
-
+        
+        
         if (entorno.estaPresionada(entorno.TECLA_ARRIBA)) {
             personaje.moverArriba();
             seMovio = true;
@@ -45,16 +70,20 @@ public class Juego extends InterfaceJuego {
 
         
         if (!seMovio) {
-            personaje.quedarseQuieto();
+            personaje.quedarseQuieto(); // si personaje no se movio, se queda quieto, es decir la tecla no esta presionada.(no hay animacion)
         }
 
         personaje.dibujar(entorno);
+        
     }
-
-
     
 
+    
+    @SuppressWarnings("unused")
+
     public static void main(String[] args) {
-        new Juego();
+    	
+    	
+        Juego juego = new Juego();
     }
 }
--- src/juego/Personaje.java
+++ src/juego/Personaje.java
@@ -21,14 +21,14 @@ public class Personaje {
     public Image enkiQuietoAbajo;
     public Image enkiQuietoIzquierda;
     public Image enkiQuietoDerecha;
-
-
+	
+    // carga de variables de instancia (posicion, velocoidad, animaciones e imagenes del personaje quieto).
 
     public Personaje(int x, int y) {
     	
         this.x = x;
         this.y = y;
-        this.velocidad = 2;
+        this.velocidad = 5;
         
         this.enkiArriba = Herramientas.cargarImagen("images/personaje/supgif.gif");
         this.enkiAbajo = Herramientas.cargarImagen("images/personaje/gif2.gif");
@@ -42,21 +42,43 @@ public class Personaje {
         this.imagenActual = enkiQuietoAbajo; 
     }
     
-    public void quedarseQuieto() {
+    public void quedarseQuieto() { // funcion quedarse quieto que setea la imagen actual en base a la ultima posicion que haya tenido el personaje y pone la imagen quieta.
         if (imagenActual == enkiIzquierda) imagenActual = enkiQuietoIzquierda;
         else if (imagenActual == enkiDerecha) imagenActual = enkiQuietoDerecha;
         else if (imagenActual == enkiArriba) imagenActual = enkiQuietoArriba;
         else if (imagenActual == enkiAbajo) imagenActual = enkiQuietoAbajo;
     }
     
-
+    
     
     // Métodos para mover al personaje
-    public void moverIzquierda() { this.x -= velocidad; this.imagenActual = this.enkiIzquierda;}
-    public void moverDerecha() { this.x += velocidad; this.imagenActual = this.enkiDerecha;}
-    public void moverArriba() {this.y -= velocidad; this.imagenActual = this.enkiArriba;}
-    public void moverAbajo() { this.y += velocidad; this.imagenActual = this.enkiAbajo; }
+    public void moverIzquierda() {
+        if (this.x - velocidad > 0 +20)
+            this.x -= velocidad;
+        this.imagenActual = this.enkiIzquierda;
+    }
 
+    public void moverDerecha() {
+        if (this.x + velocidad < 1400 -216) // Ancho de la pantalla
+            this.x += velocidad;
+        this.imagenActual = this.enkiDerecha;
+    }
+
+    public void moverArriba() {
+        if (this.y - velocidad > 0 +40)
+            this.y -= velocidad;
+        this.imagenActual = this.enkiArriba;
+    }
+
+    public void moverAbajo() {
+        if (this.y + velocidad < 900 -40 ) // Alto de la pantalla
+            this.y += velocidad;
+        this.imagenActual = this.enkiAbajo;
+    }
+
+
+    
+    
     // Dibuja el personaje
     public void dibujar(Entorno entorno) {
         entorno.dibujarImagen(imagenActual, x, y, 0);
